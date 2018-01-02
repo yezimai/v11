eda21424610db37d4c28bc35649764731c3dd021
yezimai <41815224@qq.com>
2018-01-03 00:48:08 +0800

    修改增加显示对应项目详情菜单

--- op_app/Model/getProjectDetailModelClass.py
+++ op_app/Model/getProjectDetailModelClass.py
@@ -5,6 +5,7 @@
 from op_app.Model.base.baseModelClass import BaseDbModelClass
 from op_app.logger.log import dblog
 import sys
+import json
 
 class GetProjectDetailModelClass(BaseDbModelClass):
 
@@ -37,7 +38,12 @@ class GetProjectDetailModelClass(BaseDbModelClass):
         if len(server_type) == 0 :
             print('wrong server_type11')
             return ''
+        res1=()
+        res2=()
         for i in server_type:
+            env_list = []
+            app_list = []
+
             if int(i[0]) == 2: #虚拟机
                 sql = '''
                 select DISTINCT p.name,p.`code`,p.id as project_id,h.id as env_id,
@@ -56,57 +62,95 @@ class GetProjectDetailModelClass(BaseDbModelClass):
                                         (SELECT a.pid from op_app_nav a where a.id=%s))
                 '''
                 try:
-                    res = self._cursorQuery(sql, [self.nav_id])
+                    res1 = self._cursorQuery(sql, [self.nav_id])
                 except Exception as e:
                     dblog.error("[ERROR] Query error, Catch exception:[ %s ], file: [ %s ], line: [ %s ]" % (
                         e, __file__, sys._getframe().f_lineno))
-                if len(res) == 0:
-                    print('wrong server_type12')
+                if len(res1) == 0:
+                    print('wrong server_info-res1')
                     return ''
-                for v in res:
-                    v_dic = dict()
-                    v_dic['project_name'] = v[0]
-                    v_dic['project_code'] = v[1]
-                    v_dic['project_id'] = v[2]
-                    app_dic = dict()
-                    app_dic['id'] = v[4]
-                    app_dic['name'] = v[5]
-                    app_list = []
-                    app_list.append(app_dic)
-                    v_dic['apps'] = app_list
-                    env_dic = dict()
-                    env_dic['id'] = v[3]
-                    env_id = v[6]
-                    env_dic['selected'] = 0
-                    env_type_choices={
-                        1:'dev',
-                        2:'sit',
-                        3:'uat',
-                        4:'pre',
-                        5:'prd',
-                    }
-                    if env_id in env_type_choices:
-                        env_dic['name'] = env_type_choices[env_id]
-                    env_list = []
-                    env_list.append(env_dic)
-                    v_dic['envs'] = env_list
-                    print('<DDDDDDDDDDDD------>\033[32;1m%s\033[0m' % dict_final)
+                print('res111',res1,type(res1))
+
+            else:
+                sql = '''
+                    select DISTINCT p.name,p.`code`,p.id as project_id,h.id as env_id,
+                          e.id as app_id,e.`name`,h.env_type
+                    from cmdb_project p, -- 项目表	
+                        cmdb_app e , 	-- APP表
+                        cmdb_server_app de , -- 服务器-app-中间表
+                        cmdb_physical_server d, -- 物理机
+                        cmdb_env h,	-- 环境表
+                        cmdb_env_server hd -- 环境-服务器-中间表
+                    where e.project_id= p.id and e.id=de.app_id 
+                        and d.id=de.physical_server_id and d.id=hd.physical_server_id and hd.env_id=h.id
+                        and p.code=
+                                (select aa.code from op_app_nav aa 
+                                where aa.id=
+                                            (SELECT a.pid from op_app_nav a where a.id=%s))
+                    '''
+                try:
+                    res2 = self._cursorQuery(sql, [self.nav_id])
+                    print('res2------',res2)
+                except Exception as e:
+                    dblog.error("[ERROR] Query error, Catch exception:[ %s ], file: [ %s ], line: [ %s ]" % (
+                        e, __file__, sys._getframe().f_lineno))
+                if len(res2) == 0:
+                    print('wrong server_info-res2')
+                    return ''
+        res1+=res2
+        print('whole-res,',res1)
+        for v in res1:
+            v_dic = dict()
+            v_dic['project_name'] = v[0]
+            v_dic['project_code'] = v[1]
+            v_dic['project_id'] = v[2]
+            app_dic = dict()
+            app_dic['id'] = v[4]
+            app_dic['name'] = v[5]
+            env_dic = dict()
+            env_dic['id'] = v[3]
+            env_id = v[6]
+            env_dic['selected'] = 0
+            env_type_choices={
+                '1':'dev',
+                '2':'sit',
+                '3':'uat',
+                '4':'pre',
+                '5':'prd',
+            }
+            if env_id in env_type_choices:
+                env_dic['name'] = env_type_choices[env_id]
+            # env_list = []
+            if env_dic not in env_list:
+                env_list.append(env_dic)
+
+                print('<FFFFFFFFFFFFFF------>\033[32;1m%s\033[0m' % env_list)
+
+            # app_list = []
+
+            if app_dic not in app_list:
+                app_list.append(app_dic)
 
 
+        v_dic['envs'] = env_list
+        v_dic['apps'] = app_list
+        print('<DDDDDDDDDDDD------>\033[31;1m%s\033[0m' % v_dic)
 
-        # res_dic = {}
-        # r_list=[]
-        #
-        # app_list=[]
-        # for v in data:
-        #     r_dic = {}
-        #     res_dic['project_name'] = v[0]
-        #     res_dic['project_code'] = v[1]
-        #     res_dic['project_id'] = v[2]
-        #     r_dic['name']=v[3]
-        #     r_dic['id']=v[4]
-        #     r_list.append(r_dic)
-        #     res_dic['apps']=r_list
+        dblog.info("v_dic88888 last: %s,file: [ %s ], line: [ %s ]" % (
+            json.dumps(v_dic, indent=4), __file__, sys._getframe().f_lineno))
+        return v_dic
+    # r_list=[]
+    #
+    # app_list=[]
+    # for v in data:
+    #     r_dic = {}
+    #     res_dic['project_name'] = v[0]
+    #     res_dic['project_code'] = v[1]
+    #     res_dic['project_id'] = v[2]
+    #     r_dic['name']=v[3]
+    #     r_dic['id']=v[4]
+    #     r_list.append(r_dic)
+    #     res_dic['apps']=r_list
         #     #app_list.append(v[4])
         # print('rdic<------>\033[32;1m%s\033[0m' % res_dic)
         # #print('app_list<------>\033[32;1m%s\033[0m' % app_list)
@@ -145,3 +189,4 @@ class GetProjectDetailModelClass(BaseDbModelClass):
         # print('resdic<------>\033[34;1m%s\033[0m' % res_dic)
         #return res_dic
         #return r_list
+
